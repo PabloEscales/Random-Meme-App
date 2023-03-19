734258c51f5182a9097a74d6146e6f4969d79028
Pablo Escales <pabloescales@gmail.com>
2023-03-19 13:54:19 +0100

    useState query added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import SearchBar from './Components/SearchBar';
 
-function App() {
+export const App = () => {
+
+  const GIPHY_API_KEY = "2cZkiFTqyiS79UdSapL6LHWlublpl7iy";
+
+  const [memeGif, setMemeGif] = useState("");
+  const [query, setQuery] = useState("Type here");
+
+  const callGiphyAPI = (string) => {
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${string}&api_key=${GIPHY_API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+
+        setMemeGif(data.data[0].images.original.url)
+
+      });
+  };
+
+    callGiphyAPI(query);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <SearchBar setQuery={setQuery} />
+      </div>
+      <div
+        className="App"
+        style={{ display: "flex", gap: "20px", alignItems: "center" }}
+      >
+        <img
+          className="meme--image"
+          src={memeGif}
+          alt="meme"
+          style={{
+            objectFit: "contain",
+            width: "500px",
+            heigth: "500px"
+          }}
+        />
+        <h1>Your last before search: {query}</h1>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
